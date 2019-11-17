7ec26a17cd44dad169a4e69f5898defb10445013
Andrew Wei <andrewwei1999@gmail.com>
2019-11-17 15:27:28 -0800

    adding some code to add noise to images

--- main.py
+++ main.py
@@ -20,12 +20,40 @@ def UI_display(name=None):
 
     return render_template('landing.html', image=image_en.decode('ascii'), noise=noise_en.decode('ascii'))
 
-def generate_noise(width=256, height=256):
+# @app.route('/generate_new_noise')
+def generate_noise(width=32, height=32, scale=50/255):
     image = Image.new("RGB", (width, height), 255)
-    random_grid = list(map(lambda x: (int(random() * 256), int(random() * 256), int(random() * 256)), [0] * width * height))
+    random_grid = [*map(lambda x: (int((random() * 2 - 1) * 256 * scale), int((random() * 2 - 1) * 256 * scale), int((random() * 2 - 1) * 256 * scale)) , [0] * width * height)]
     image.putdata(random_grid)
+    print(random_grid)
     return image
+    # return random_grid
+
+# background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    print("Hello")
+    return render_template('blank.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
-
+    image = Image.open('pictures/husky.jpg')
+    width, height = image.size
+    noise = generate_noise(width, height).load()
+    img = image.load()
+    for x in range(image.size[0]):
+        for y in range(image.size[1]):
+            r, g, b = img[x, y]
+            r_a, g_a, b_a = noise[x, y]
+            r += r_a 
+            g += g_a
+            b += b_a
+            if r < 0: r = 0
+            if g < 0: g = 0
+            if b < 0: b = 0
+            if r > 255: r = 255
+            if g > 255: g = 255
+            if b > 255: b = 255
+            
+            img[x, y] = (r, g, b)
+    image.save('henlo.jpg', format='JPEG')
+    # app.run(debug=True, host='0.0.0.0')
