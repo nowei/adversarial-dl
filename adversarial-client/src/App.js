a7f2ba140d6a2657ff34e3d4d606a8ebcec726d0
Kushal Jhunjhunwalla <kushal.jhunjhunwalla@gmail.com>
2019-11-24 18:43:57 -0800

    Clean frontend

--- adversarial-client/src/App.js
+++ adversarial-client/src/App.js
@@ -1,40 +1,115 @@
 import React from 'react';
-import ImageUploader from 'react-images-upload';
 import Dropdown from './Dropdown'
+import Slider from 'react-input-slider';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
  
 class App extends React.Component {
     constructor(props) {
         super(props);
-         this.state = { pictures: [], target: -1 };
-         this.onDrop = this.onDrop.bind(this);
-         this.onSelect = this.onSelect.bind(this);
+        this.state = {
+                    target: -1, eps:  (4).toFixed(1), 
+                    defaultTarget:{ label: 'None', value: -1 }};
+        this._handleImageChange = this._handleImageChange.bind(this);
+        this._handleSubmit = this._handleSubmit.bind(this);
     }
- 
-    onDrop(picture) {
-        this.setState({
-            pictures: this.state.pictures.concat(picture),
+
+    _handleImageChange(e) {
+        e.preventDefault();
+
+        let reader = new FileReader();
+        let file = e.target.files[0];
+
+        reader.onloadend = () => {
+            this.setState({
+                file: file,
+                imagePreviewUrl: reader.result
+            });
+        }
+
+        reader.readAsDataURL(file)
+    }
+
+    _handleSubmit(e) {
+        e.preventDefault();
+        console.warn(this.state)
+        // this.upload(this.state.file)
+    }
+
+    upload(imageFile) {
+        return new Promise((resolve, reject) => {
+            let imageFormData = new FormData();
+        
+            imageFormData.append('imageFile', imageFile);
+            
+            var xhr = new XMLHttpRequest();
+            
+            xhr.open('post', '/upload', true);
+            
+            xhr.onload = function () {
+            if (this.status === 200) {
+                resolve(this.response);
+            } else {
+                reject(this.statusText);
+            }
+            };
+            
+            xhr.send(imageFormData);
         });
     }
 
-    onSelect(option) {
-    //   console.error(option)
-      this.setState({
-        target: option.value,
-      });
-    }
- 
     render() {
+        const {imagePreviewUrl} = this.state;
+        let $imagePreview = null;
+        if (imagePreviewUrl) {
+            $imagePreview = (
+                    <div className="imgPreview">
+                        <img src={imagePreviewUrl} alt={'Error in rendering uploaded pic'}/>
+                    </div>
+            )
+        }
         return (
             <div>
-              <ImageUploader
-                  withIcon={true}
-                  buttonText='Choose images'
-                  onChange={this.onDrop}
-                  imgExtension={['.jpg', '.gif', '.png']}
-                  maxFileSize={5242880}
-                  withPreview={true}
-              />
-              <Dropdown onChange={this.onSelect}/>
+                <Container>
+                    <Row>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label>Please upload an image:</Form.Label>
+                                <Form.Control type="file" placeholder="" accept="image/jpeg,image/png,image/jpg" onChange={this._handleImageChange}/>
+                            </Form.Group>
+
+                            <Form.Group>
+                                {/* <Form.Label>{'Please select a target: ' + this.state.target}</Form.Label> */}
+                                <Form.Label>Please select a target:</Form.Label>
+                                <Dropdown onChange={(option) => this.setState({ target: option.value })} defaultValue={this.state.defaultTarget}/>
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>{'Please select perturbation contant: ' + this.state.eps + '/255'}</Form.Label>
+                                <br/>
+                                <Slider
+                                    axis="x"
+                                    xstep={0.5}
+                                    xmin={0} // TODO: change to 1
+                                    xmax={15}
+                                    x={this.state.eps}
+                                    onChange={({ x }) => this.setState({ eps: parseFloat(x.toFixed(1)).toFixed(1) })}
+                                    />
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit" onClick={this._handleSubmit}>
+                                Submit
+                            </Button>
+                        </Form>
+                    </Row>
+                    <Row>
+                        {$imagePreview}
+                    </Row>
+                </Container>
             </div>
         );
     }
--- adversarial-client/src/Dropdown.js
+++ adversarial-client/src/Dropdown.js
@@ -4,13 +4,13 @@ import Select from 'react-select'
 const allVals = require('./data.json');
 
 const options = Object.keys(allVals).map(function(key, index) {
-  return {value: key, label: allVals[key]}
+    return {value: key, label: allVals[key]}
 });
 
 class Dropdown extends Component {
-  render() {
-    return <Select options={options} isSearchable={true} onChange={this.props.onChange}/>;
-  }
+    render() {
+        return <Select options={options} isSearchable={true} onChange={this.props.onChange} defaultValue={this.props.defaultValue}/>;
+    }
 }
 
 export default Dropdown;
